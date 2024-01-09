a714c7cca1d558ecc148611b26bca5ded31dbefc
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2024-01-09 10:30:33 -0300

    feat: install docker compose

---
+++ src/main/java/br/com/gildairmoreira/gestaovagas/exceptions/ErrorMessageDTO.java
@@ -0,0 +1,11 @@
+package br.com.gildairmoreira.gestaovagas.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessageDTO {
+    private String message;
+    private String field;
+}
--- src/main/java/br/com/gildairmoreira/gestaovagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/gildairmoreira/gestaovagas/modules/candidate/CandidateEntity.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.hibernate.validator.constraints.Length;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
@@ -13,13 +14,14 @@ public class CandidateEntity {
     private UUID id;
     private String name;
 
-    @Pattern(regexp = "^(?!\\s*$).+", message = "O Campo [username] não deve conter espaço")
+    @NotBlank()
+    @Pattern(regexp = "\\S+", message = "O Campo [username] não deve conter espaço")
     private String username;
 
     @Email(message = "O campo [email] deve conter um email válido")
     private String email;
 
-    @Length(min = 10 , max = 100)
+    @Length(min = 10 , max = 100, message = "A sennha deve conter entre (10) e (100) caracteres")
     private String password;
     private String descriptions;
     private String curriculum;
