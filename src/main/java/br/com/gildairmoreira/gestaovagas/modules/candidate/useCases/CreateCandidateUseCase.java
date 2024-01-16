a97d0b1ab056bfb0919d5efb70a3443c486552a3
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2024-01-16 18:00:55 -0300

    feat: "adicionando criptografia nos candidatos cadastrados"

--- src/main/java/br/com/gildairmoreira/gestaovagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/br/com/gildairmoreira/gestaovagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -1,6 +1,7 @@
 package br.com.gildairmoreira.gestaovagas.modules.candidate.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.gildairmoreira.gestaovagas.exceptions.UserFoundExeption;
@@ -13,12 +14,18 @@ public class CreateCandidateUseCase {
     @Autowired
     private CandidateRepository candidateRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CandidateEntity execute(CandidateEntity candidateEntity) {
         this.candidateRepository
                 .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
                 .ifPresent((user) -> {
                     throw new UserFoundExeption();
                 });
+
+        var password = passwordEncoder.encode(candidateEntity.getPassword());
+        candidateEntity.setPassword(password); 
 
         return this.candidateRepository.save(candidateEntity);
     }
