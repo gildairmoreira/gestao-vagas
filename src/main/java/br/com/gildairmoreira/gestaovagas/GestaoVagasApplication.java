4661a653b27f81b3d06c23462ccb899645e94ed5
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2024-01-23 10:56:01 -0300

    feat: adicionando o swagger na api e configurando as rotas no securityconfig para liberar o swagger

--- src/main/java/br/com/gildairmoreira/gestaovagas/GestaoVagasApplication.java
+++ src/main/java/br/com/gildairmoreira/gestaovagas/GestaoVagasApplication.java
@@ -3,7 +3,13 @@ package br.com.gildairmoreira.gestaovagas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Gestão de Vagas", description = "API responsável pela gestão de Vagas", version = "1"
+
+))
 public class GestaoVagasApplication {
 
 	public static void main(String[] args) {
